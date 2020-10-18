6a98924cde66d18b274692037b0b9ee72f696feb
Harsha <harsha.sam23@gmail.com>
2020-10-18 10:19:24 +0530

    18 OCT 2020 Added Efficient sol to prb 34 using binary search

--- Arrays/34.  Find First and Last Position of Element in Sorted Array.py
+++ Arrays/34.  Find First and Last Position of Element in Sorted Array.py
@@ -3,7 +3,8 @@
 # Medium
 
 class Solution:
-#     Linear search : O(n)
+#   Linear search : O(n)
+#   Space Complexity: O(1)
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         start, end = -1, -1
         flag = 0
@@ -16,3 +17,40 @@ class Solution:
                 else:
                     end = index
         return start, end
+
+
+# Binary search
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # Finding first pos         
+        l = 0
+        h = len(nums) - 1
+        pos = [-1, -1]
+        while l <= h:
+            mid = (l + h) // 2
+            if nums[mid] == target:
+                pos[0] = mid
+                h = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                h = mid - 1
+
+        # Finding last pos                         
+        if pos[0] != -1:                  
+            l = pos[0]
+            h = len(nums) - 1
+            while l <= h:
+                mid = (l + h) // 2
+                if nums[mid] == target:
+                    pos[1] = mid
+                    l = mid + 1
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    h = mid - 1
+        return pos
+                
+        
