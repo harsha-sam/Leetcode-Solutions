c38fe725e916a769a5a775320f05f8ee5713f562
Harsha <harsha.sam23@gmail.com>
2021-04-16 12:59:11 +0530

    Added solution to problem 152 Maximum Product Subarray.py

---
+++ Arrays/152. Maximum Product Subarray.py
@@ -0,0 +1,15 @@
+# Medium
+# https://leetcode.com/problems/maximum-product-subarray/
+# TC: O(N)
+# SC: O(1)
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        maxi, mini, out = nums[0], nums[0], nums[0]
+        for num in nums[1:]:
+            temp = maxi
+            maxi = max(num, num * maxi, num * mini)
+            mini = min(num, num * temp, num*mini)
+            print(maxi, mini)
+            out = max(out, maxi)
+        return out
+    
