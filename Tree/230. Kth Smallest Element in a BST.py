3d5a5d2d9b2565f9de07dd5b2f7dd52cbc5b42d9
Harsha <harsha.sam23@gmail.com>
2021-06-27 08:11:04 +0530

    Changed solution to prb kth smallest element in a bst

--- Tree/230. Kth Smallest Element in a BST.py
+++ Tree/230. Kth Smallest Element in a BST.py
@@ -9,14 +9,35 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
     def kthSmallest(self, root: TreeNode, k: int) -> int:
-        def inorder(node, order):
-            if node:
-                inorder(node.left, order)
-                order.append(node.val)
-                inorder(node.right, order)
+        def inorder(node):
+            if not node:
+                return None
+            nonlocal k
+            left = inorder(node.left)
+            if left is not None:
+                return left
+            k -= 1
+            if k == 0:
+                return node.val
+            return inorder(node.right)
+
+        ans = inorder(root)
+        if ans is None:
+            return -1
+        return ans
+
+# class Solution:
+#     def kthSmallest(self, root: TreeNode, k: int) -> int:
+#         def inorder(node, order):
+#             if node:
+#                 inorder(node.left, order)
+#                 order.append(node.val)
+#                 inorder(node.right, order)
         
-        order = []
-        inorder(root, order)
-        return order[k - 1]
+#         order = []
+#         inorder(root, order)
+#         return order[k - 1]
