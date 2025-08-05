095e767f58997165a147d3885ff7f4adb533b406
Harsha <harsha.sam23@gmail.com>
2025-08-04 22:53:20 -0400

    updated 3

--- Arrays/3. Longest Substring Without Repeating Characters.py
+++ Arrays/3. Longest Substring Without Repeating Characters.py
@@ -9,17 +9,16 @@
 # https://www.youtube.com/watch?v=eS6PZLjoaq8 (Minimum Window Substring)
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        hash_map = {}
-        for char in s:
-            hash_map[char] = 0
-        start, end = 0, 0
-        longest_substring = 0
-        while start < len(s):
-            if hash_map[s[start]] > 0:
-                hash_map[s[start]] -= 1
-                start += 1
-            while end < len(s) and hash_map[s[end]] == 0:
-                hash_map[s[end]] += 1
-                end += 1
-            longest_substring = max(longest_substring, end - start)
-        return longest_substring
+        count = {}
+        l, r = 0, 0
+        maxLen = 0
+
+        while r < len(s):
+            while r < len(s) and count.get(s[r], 0) == 0:
+                count[s[r]] = count.get(s[r], 0) + 1
+                r += 1
+            maxLen = max(maxLen, r-l)
+            count[s[l]] = count.get(s[l]) - 1
+            l += 1
+
+        return maxLen
