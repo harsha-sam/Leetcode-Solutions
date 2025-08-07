6c0913b11217fd7f064ea61082b4f78dd99d0a5a
Harsha <harsha.sam23@gmail.com>
2025-08-06 21:28:11 -0400

    prfix trie and bst

--- Strings/208. Implement Prefix Trie.py
+++ Strings/208. Implement Prefix Trie.py
@@ -1,54 +1,38 @@
-# Medium
-# https://leetcode.com/problems/implement-trie-prefix-tree/
 class TrieNode:
-    def __init__(self, text='', isWord=False):
-        self.text = text
+    def __init__(self, val=""):
+        self.val = val
         self.children = {}
-        self.isWord = isWord
+        self.isWord = False
+
 
 class Trie:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
         curr = self.root
-        for letter in word:
-            if letter not in curr.children:
-                curr.children[letter] = TrieNode(letter)
-            curr = curr.children[letter]
+        for char in word:
+            if not (char in curr.children):
+                curr.children[char] = TrieNode(char)
+            curr = curr.children[char]
         curr.isWord = True
 
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
         curr = self.root
-        for letter in word:
-            if letter not in curr.children:
+        for char in word:
+            if not (char in curr.children):
                 return False
-            curr = curr.children[letter]
+            curr = curr.children[char]
         return curr.isWord
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
         curr = self.root
-        for letter in prefix:
-            if letter not in curr.children:
+        for char in prefix:
+            if not (char in curr.children):
                 return False
-            curr = curr.children[letter]
-        
-        if curr.children or curr.isWord:
-            return True 
-        return False
+            curr = curr.children[char]
+        return True
 
 
 # Your Trie object will be instantiated and called as such:
--- Tree/98. Validate Binary Search Tree.py
+++ Tree/98. Validate Binary Search Tree.py
@@ -1,31 +1,14 @@
-# Medium
-# https://leetcode.com/problems/validate-binary-search-tree/
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# Time Complexity: O(N)
-# Space Complexity: O(h)
-import math
-
 class Solution:
-    def isValidBST(self, root: TreeNode) -> bool:
-        return self.explore(root, -math.inf, math.inf) 
-    
-    def explore(self, node, mini, maxi):
-        if node:
-            if node.val <= mini or node.val >= maxi:
-                return False
-            if not self.explore(node.left, mini, node.val):
-                return False
-            if not self.explore(node.right, node.val, maxi):
-                return False
-        return True
-    
-            
-            
-        
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def helper(node, mini, maxi):
+            if not node:
+                return True
+            return node and mini < node.val < maxi and helper(node.left, mini, node.val) and helper(node.right, node.val, maxi)
+
+        return helper(root, -math.inf, math.inf)
